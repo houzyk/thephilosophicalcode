13b6b5d7f10b208ed42c3098740b7d0c0f2180bf
houzairkoussa <houzairmk@icloud.com>
2023-07-28 16:46:24 +0400

    [dev] ADD og image

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "The Philosophical Code";
-export const SITE_DESCRIPTION = "Open-source article series exploring the intersection of philosophy and computation";
+export const SITE_DESCRIPTION = "Exploring Code From A Philosophical Point Of View";
 export const TWITTER_HANDLE = "@yourtwitterhandle";
 export const MY_NAME = "Captain Awesome";
 
