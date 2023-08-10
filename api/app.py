1a0379835e152a9f4a1710a152e3863ff0adf044
houzairkoussa <houzairmk@icloud.com>
2023-08-10 21:38:23 +0400

    [dev] INIT api

---
+++ api/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+PEOPLE_FILE_PATH = "../content/people.json"
+
+@app.route("/")
+def index():
+	return {
+		"routes": {
+			"people": "/people",
+            "stuff": "/stuff",
+            "articles": "/articles",
+		}
+	}
+
+@app.route("/people")
+def people():
+	return send_file(path_or_file = PEOPLE_FILE_PATH)
+
+@app.route("/stuff")
+def stuff():
+	return ""
+
+@app.route("/articles")
+def articles():
+	return ""
