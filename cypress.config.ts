830a989626ec84f65e565b708fadf8df114d025c
houzairkoussa <houzairmk@icloud.com>
2023-08-16 21:11:53 +0400

    [cicd] ADD preview url to cypress env

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-    },
-    env: {
-      vercel_preview_url: process.env.VERCEL_PREVIEW_URL
     }
+  },
+  env: {
+    vercel_preview_url: process.env.VERCEL_PREVIEW_URL
   }
 });
