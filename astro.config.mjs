4d3c76d796b89f20f7338e6acbfeb63fe488f465
houzairkoussa <houzairmk@icloud.com>
2023-12-15 21:34:16 +0400

    Revert "add vercel analytics"
    This reverts commit b297925c97ef5238d41758ace8cbd20c17741c71.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,26 @@
 /* eslint-disable turbo/no-undeclared-env-vars */
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
+
 const SERVER_PORT = 3000;
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 const LIVE_URL = "https://thephilosophicalcode.com";
+
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
+
 let BASE_URL = LOCALHOST_URL;
+
 if (isBuild) {
   BASE_URL = LIVE_URL;
 }
+
 if (process.env.VERCEL_ENV === "preview" || process.env.VERCEL_ENV === "development") {
   BASE_URL = `https://${process.env.VERCEL_URL}`;
 }
 
-// https://astro.build/config
 export default defineConfig({
-  server: {
-    port: SERVER_PORT
-  },
+  server: { port: SERVER_PORT },
   site: BASE_URL,
-  integrations: [sitemap()],
-  output: "static",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true
-    },
-    speedInsights: {
-      enabled: true
-    }
-  })
+  integrations: [sitemap()]
 });
