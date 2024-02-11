608132095ea2239a2e8c5c181d1c201666bd6f88
houzairkoussa <houzairmk@icloud.com>
2024-02-11 19:00:54 +0400

    dev(analytics) add vercel web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 /* eslint-disable turbo/no-undeclared-env-vars */
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
+import vercel from "@astrojs/vercel/static";
 const SERVER_PORT = 3000;
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 const LIVE_URL = "https://thephilosophicalcode.com";
-
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
-
 let BASE_URL = LOCALHOST_URL;
 
 if (isBuild) {
@@ -20,7 +18,15 @@ if (process.env.VERCEL_ENV === "preview" || process.env.VERCEL_ENV === "developm
 }
 
 export default defineConfig({
-  server: { port: SERVER_PORT },
+  server: {
+    port: SERVER_PORT
+  },
   site: BASE_URL,
-  integrations: [sitemap()]
+  integrations: [sitemap()],
+  output: "static",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    }
+  }),
 });
